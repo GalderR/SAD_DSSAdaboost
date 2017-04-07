ed02f4e21def89909dd32eeaa4cfe25eb5fd05a7
Duque <Duque@10.109.234.125>
2017-04-07 18:18:02 +0200

    Data metodo append y en TfIdf pasar los atributos a nominal

--- SAD_AdaBoost/src/preprocesamiento/fssTfIdf/TfIdf.java
+++ SAD_AdaBoost/src/preprocesamiento/fssTfIdf/TfIdf.java
@@ -1,8 +1,12 @@
 package preprocesamiento.fssTfIdf;
 
 import code.Data;
+import code.Preprocess;
+import weka.attributeSelection.CfsSubsetEval;
 import weka.core.Instances;
 import weka.filters.Filter;
+import weka.filters.supervised.attribute.AttributeSelection;
+import weka.filters.unsupervised.attribute.NumericToNominal;
 import weka.filters.unsupervised.attribute.StringToWordVector;
 import weka.filters.unsupervised.instance.NonSparseToSparse;
 
@@ -35,15 +39,42 @@ public class TfIdf {
 		Instances sparsedDev = Filter.useFilter(bowDev, filter2);
 		Instances sparsedTest = Filter.useFilter(bowTest, filter2);
 		System.out.println("FIN de Sparse...");
+		//Numeric To Nominal
+		System.out.println("Aplicando Numeric To Nominal...");
+		NumericToNominal filter3 = numericToNominal(sparsedTrain);
+		Instances nominalTrain = Filter.useFilter(bowTrain, filter3);
+		Instances nominalDev = Filter.useFilter(bowDev, filter3);
+		Instances nominalTest = Filter.useFilter(bowTest, filter3);
+		System.out.println("FIN de Numeric To Nominal...");
+		
+		double idf;
+		int cont = 0;
+		for (int i = 1; i < nominalTrain.numAttributes()-1; i++) {
+			idf = Double.parseDouble(nominalTrain.attribute(i).value(1));
+			if (idf > 3) {// Observando los resultados vemos que cuánto más
+							// pequeño es el número idf más común es la palabra
+							// en las 3 clases.
+				nominalTrain.deleteAttributeAt(i);
+				i--;
+				cont++;
+			}
+		}
 
 		// Print timer
 		Long tFin = System.currentTimeMillis();
 		System.out.println("Tiempo BOW : " + (tFin - tInicio) + " milisegundos");
+		
+		Instances fixedDev = Preprocess.headerFixing(nominalTrain, nominalDev);
+		Instances fixedTest = Preprocess.headerFixing(nominalTrain, nominalTest);
+		
+		//TODO borrar
+		Instances kk = Data.getData().append(nominalTrain, fixedDev);
+		Data.getData().generateArff(Data.getData().formatearPath(args[0]) + "\\kk.arff", kk);
 
 		// Exportar a ARFF
-		Data.getData().generateArff(Data.getData().formatearPath(args[0]) + "\\trainBOW_FSS_TFIDF.arff", sparsedTrain);
-		Data.getData().generateArff(Data.getData().formatearPath(args[1]) + "\\devBOW_FSS_TFIDF.arff", sparsedDev);
-		Data.getData().generateArff(Data.getData().formatearPath(args[2]) + "\\testBOW_FSS_TFIDF.arff", sparsedTest);
+		Data.getData().generateArff(Data.getData().formatearPath(args[0]) + "\\trainBOW_FSS_TFIDF.arff", nominalTrain);
+		Data.getData().generateArff(Data.getData().formatearPath(args[1]) + "\\devBOW_FSS_TFIDF.arff", fixedDev);
+		Data.getData().generateArff(Data.getData().formatearPath(args[2]) + "\\testBOW_FSS_TFIDF.arff", fixedTest);
 	}
 	
 	private static NonSparseToSparse nonSparseToSparse(Instances data) throws Exception{
@@ -63,6 +94,13 @@ public class TfIdf {
 		filter.setOutputWordCounts(true);
 		filter.setInputFormat(data);
 
+		return filter;
+	}
+	
+	public static NumericToNominal numericToNominal(Instances data) throws Exception {
+		NumericToNominal filter = new NumericToNominal();
+		filter.setAttributeIndices("2-last-1");
+		filter.setInputFormat(data);
 		return filter;
 	}
 
